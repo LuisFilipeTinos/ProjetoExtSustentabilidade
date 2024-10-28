a72fb414beb59e2f2e2ef002355f792fef32c524
Nicolas3077 <nicodev.informatica@gmail.com>
2024-10-27 21:26:09 -0300

    Criação da Movimentação, Câmera e UI

---
+++ Assets/Scripts/PlayerControll2.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControll2 : MonoBehaviour
+{
+    [SerializeField] private float speed2 = 8f; //Define a velocidade
+    private float horizontal2; //Pega o movimento horizontal
+    private Rigidbody2D rb2;
+
+    private string horizontalInputAxis2 = "Horizontal2";
+
+    [SerializeField] private Transform groundCheck2; //Suporte que está nos pés do personagem
+    [SerializeField] private LayerMask groundLayer2; //Layer do chão (GROUND)
+
+    private bool isGrounded2; //Verificar se o personagem está no chão
+
+    private void Awake()
+    {
+        rb2 = GetComponent<Rigidbody2D>(); //Pega o RigidBody2d
+    }
+
+    private void Update() //Método que será chamado a cada frame por segundo
+    {
+
+        horizontal2 = Input.GetAxis(horizontalInputAxis2); //Pega a movimentação do personagem do eixo X
+
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded2) //Verifica se a seta pra cima foi pressionada e se está no chão
+        {
+            rb2.AddForce(Vector2.up * 600);
+        }
+
+        isGrounded2 = Physics2D.OverlapCircle(groundCheck2.position, 0.2f, groundLayer2); //Verifica se algo está no chão, em um circulo invisivel
+        //Caso esteja no chão, ele resulta em TRUE, se não, resultará em FALSE (significando que está fora do chão)
+    }
+
+    private void FixedUpdate() //Método para manipular a física
+    {
+        rb2.velocity = new Vector2(horizontal2 * speed2, rb2.velocity.y); //Lógica da física
+    }
+
+}
