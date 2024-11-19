3eb585a083817659fe09d640a723eb332d0d0b1d
jvpdmelo <jvpdmelo@gmail.com>
2024-11-18 23:08:25 -0300

    Fase 2 completa e sistema de pause

--- Assets/Scripts/ScenesScripts/GameOverController.cs
+++ Assets/Scripts/ScenesScripts/GameOverController.cs
@@ -14,17 +14,25 @@ public class GameOverscene : MonoBehaviour
     [SerializeField] GameObject Playertwo;
     [SerializeField] GameObject GameOvertxt;
     [SerializeField] GameObject ResumeBtt;
+    [SerializeField] GameObject PauseBtt;
 
+
+
+     void Start()
+     {
+        Time.timeScale = 1;
+     }
     void Update()
     {
 
         if (Playertwo == null && Playerone == null )
         {
-            Debug.Log("gameover ativou");
+
             MainmenuBtt.SetActive(true);
             RestartBtt.SetActive(true);
             fade.SetActive(true);
             GameOvertxt.SetActive(true);
+            PauseBtt.SetActive(false);
             Time.timeScale = 0;
         }
         
@@ -33,15 +41,28 @@ public class GameOverscene : MonoBehaviour
     public void PlayAgain(int buildindex)
     {
         SceneManager.LoadScene(buildindex);
+
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+
+        MainmenuBtt.SetActive(true);
+        RestartBtt.SetActive(true);
+        fade.SetActive(true);
+        ResumeBtt.SetActive(true);
+        PauseBtt.SetActive(false);
     }
 
     public void Resume()
     {
         Time.timeScale = 1; //Volta o tempo a correr
 
-        RestartBtt.SetActive(false);
         fade.SetActive(false);
+        RestartBtt.SetActive(false);
         ResumeBtt.SetActive(false);
         MainmenuBtt.SetActive(false);
+        PauseBtt.SetActive(true);
     }
 }
